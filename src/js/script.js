d0915ee4d97d7c4f54d960ff97945d0bfdb4b8ef
Yur1 <nik456618@gmail.com>
2023-04-26 11:52:50 +0300

    Made a lock-screen,main-screen,also made a simple animation for lock-screen

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+try {
+  setInterval(() => {
+    try {
+      document
+        .querySelector(".lock__screen")
+        .classList.remove("animate__fadeIn");
+      document
+        .querySelector(".lock__screen")
+        .classList.add("animate__fadeOutUp");
+      setInterval(() => {
+        document.querySelector(".lock__screen").style.display = "none";
+        document.querySelector(".main__screen").style.display = "block";
+        document
+          .querySelector(".main__screen")
+          .classList.add("animate__fadeIn");
+      }, 500);
+    } catch (error) {}
+  }, 2000);
+} catch (error) {}
